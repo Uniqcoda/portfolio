8a7d96f2e355e2c0a5572422951b419532cef9dd
Mary Etokwudo <etokwudomary@gmail.com>
2020-09-16 20:23:24 +0100

    add banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -4,7 +4,9 @@ export default function Banner() {
   return (
     <div className='banner'>
       <div className='bg-light'>Hey, I'm</div>  
-      <div className='bg-dark'>Mary.</div>   
+      <div className='bg-dark'>Mary.</div>
+
+      <button className='about-button'>About <i class="fas fa-chevron-down"></i></button>
     </div>
   )
 }
