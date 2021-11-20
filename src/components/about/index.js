666853c1df3344a5f0a34ea6ba6c186445371a6a
Uniqcoda <etokwudomary@gmail.com>
2021-11-20 17:33:39 +0100

    edit

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,21 +8,22 @@ export default function About() {
 			<hr />
 			<div className='about-card'>
 				<div className='about-card-image'>
-					<img loading='lazy' src='/images/profile_image.jpg' alt='profile' />
+					<img loading='lazy' src='/images/profile_image.png' alt='profile' />
 				</div>
 				<div className='about-card-text'>
 					<h4>Mary Etokwudo</h4>
 					<p>
 						A versatile tech enthusiast experienced in building world class web
-						solutions by applying up-to-date technologies and industry best practices.
-						She has worked with both small and large teams in global organisations
-						and is dedicated to her job.
+						solutions by applying up-to-date technologies and industry best
+						practices. She has worked with both small and large teams in global
+						organizations and is dedicated to her job.
 					</p>
 					<p>
-						She started her programming journey with frontend web development
-						but currently writes backend code as well.
+						She started her programming journey as a fullstack web developer,
+						spends most of her time on the backend and is currently adding
+						blockchain technology to her list of technical skills.
 					</p>
-					<p>She is also a song-writer and a great cook.</p>
+					<p>She is a great cook and she loves visiting new places.</p>
 				</div>
 			</div>
 		</div>
